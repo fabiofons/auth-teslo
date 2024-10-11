04f6183dfc0d362ccea0cc208d05aa2090ff6236
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2024-10-11 16:42:10 -0500

    connected loginform provider to login screen

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:teslo_shop/features/auth/providers/login_form_provider.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
 
@@ -48,11 +50,13 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-class _LoginForm extends StatelessWidget {
+class _LoginForm extends ConsumerWidget {
   const _LoginForm();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final loginForm = ref.watch(loginFormState);
 
     final textStyles = Theme.of(context).textTheme;
 
@@ -64,15 +68,23 @@ class _LoginForm extends StatelessWidget {
           Text('Login', style: textStyles.titleLarge ),
           const SizedBox( height: 90 ),
 
-          const CustomTextFormField(
+          CustomTextFormField(
             label: 'Correo',
             keyboardType: TextInputType.emailAddress,
+            errorMessage: loginForm.isFormPosted 
+              ? loginForm.email.errorMessage 
+              : null,
+            onChanged: ref.read(loginFormState.notifier).onEmailChanged,
           ),
           const SizedBox( height: 30 ),
 
-          const CustomTextFormField(
+          CustomTextFormField(
             label: 'Contraseña',
             obscureText: true,
+            errorMessage: loginForm.isFormPosted 
+              ? loginForm.password.errorMessage
+              : null,
+            onChanged: ref.read(loginFormState.notifier).onPasswordChanged,
           ),
     
           const SizedBox( height: 30 ),
@@ -84,7 +96,7 @@ class _LoginForm extends StatelessWidget {
               text: 'Ingresar',
               buttonColor: Colors.black,
               onPressed: (){
-
+                ref.read(loginFormState.notifier).onFormSubmit();
               },
             )
           ),
--- lib/features/auth/providers/login_form_provider.dart
+++ lib/features/auth/providers/login_form_provider.dart
@@ -2,6 +2,55 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
+
+//! 3. StateNotifierProvider - lo que se consume
+final loginFormState = StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
+
+//! 2. Como implementamos el notifier
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  LoginFormNotifier(): super(LoginFormState());
+
+  onEmailChanged( String value) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWith(
+      email: newEmail,
+      isValid: Formz.validate([newEmail, state.password])
+    );
+  }
+
+  onPasswordChanged( String value) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWith(
+      password: newPassword,
+      isValid: Formz.validate([newPassword, state.email])
+    );
+  }
+
+  onFormSubmit() {
+    _touchEveryField();
+
+    if( !state.isValid ) return;
+
+    print(state);
+
+  }
+
+  _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password])
+    );
+  }
+  
+}
+
 //! 1. Create state
 
 class LoginFormState {
@@ -48,50 +97,4 @@ class LoginFormState {
   }
 }
 
-//! 2. Como implementamos el notifier
-class LoginFormNotifier extends StateNotifier<LoginFormState> {
-  LoginFormNotifier(): super(LoginFormState());
 
-  onEmailChanged( String value) {
-    final newEmail = Email.dirty(value);
-    state = state.copyWith(
-      email: newEmail,
-      isValid: Formz.validate([newEmail, state.password])
-    );
-  }
-
-  onPasswordChanged( String value) {
-    final newPassword = Password.dirty(value);
-    state = state.copyWith(
-      password: newPassword,
-      isValid: Formz.validate([newPassword, state.email])
-    );
-  }
-
-  onFormSubmit() {
-    _touchEveryField();
-
-    if( !state.isValid ) return;
-
-    print(state);
-
-  }
-
-  _touchEveryField() {
-    final email = Email.dirty(state.email.value);
-    final password = Password.dirty(state.password.value);
-
-    state = state.copyWith(
-      isFormPosted: true,
-      email: email,
-      password: password,
-      isValid: Formz.validate([email, password])
-    );
-  }
-  
-}
-
-//! 3. StateNotifierProvider - lo que se consume
-final loginFormState   = StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
-  return LoginFormNotifier();
-});
