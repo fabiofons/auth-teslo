53f84c6d87f044fc9f6861eb5cee3cefd2984b7a
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2024-10-27 13:43:59 -0500

    fix product screen

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -1,29 +1,36 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/products/presentation/providers/providers.dart';
 
-class ProductScreen extends ConsumerStatefulWidget {
+class ProductScreen extends ConsumerWidget {
   final String productId;
 
-  const ProductScreen({required this.productId, super.key});
+  const ProductScreen({
+    required this.productId, 
+    super.key
+  });
 
   @override
-  ProductScreenState createState() => ProductScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
 
-class ProductScreenState extends ConsumerState<ProductScreen> {
-
-  @override
-  void initState() {
-    ref.read(productProvider(widget.productId).notifier);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+    final productState = ref.watch(productProvider(productId));
     return Scaffold(
-      appBar: AppBar(title: Text('Edit product'),),
-      body: Center(child: Text(widget.productId),)
+      appBar: AppBar(
+        title: Text('Edit product'),
+        actions: [
+          IconButton(
+            onPressed: (){}, 
+            icon: Icon(Icons.camera_alt_outlined))
+        ],
+      ),
+      body: Center(
+        child: Text(productState.product?.title ?? 'Loading'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.save_as_outlined),        
+      ),
     );
   }
 }
