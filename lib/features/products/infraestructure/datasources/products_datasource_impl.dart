aa95f44fcb8cb44fa2df3979339cc651bf1d4807
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2024-10-27 13:26:05 -0500

    added error handler, added method to get productById

--- lib/features/products/infraestructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infraestructure/datasources/products_datasource_impl.dart
@@ -19,9 +19,17 @@ class ProductsDatasourceImpl extends ProductsDatasource {
   }
 
   @override
-  Future<Product> getProductById(String id) {
-    // TODO: implement getProductById
-    throw UnimplementedError();
+  Future<Product> getProductById(String id) async {
+    try {
+      final response = await dio.get('/products/$id');
+      final product = ProductMapper.jsonToEntity(response.data);
+      return product;
+    } on DioException catch (e) {
+      if(e.response!.statusCode == 404) throw ProductNotFound();
+      throw Exception();
+    } catch (e) {
+      throw Exception();
+    }
   }
 
   @override
--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/presentation/providers/providers.dart';
 
 class ProductScreen extends ConsumerStatefulWidget {
   final String productId;
@@ -11,6 +12,13 @@ class ProductScreen extends ConsumerStatefulWidget {
 }
 
 class ProductScreenState extends ConsumerState<ProductScreen> {
+
+  @override
+  void initState() {
+    ref.read(productProvider(widget.productId).notifier);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
